2fbdfaaeb3d8ea04effb8e0ea63f3c1b122d6a81
jmao12 <jtnmga@gmail.com>
2022-11-02 14:17:38 -0700

    add pdf to byte array conversion to Utilites.java

--- applications/server/src/main/java/internal/service/Utilities.java
+++ applications/server/src/main/java/internal/service/Utilities.java
@@ -7,12 +7,15 @@ import com.mongodb.client.gridfs.model.GridFSUploadOptions;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.tools.imageio.ImageIOUtil;
+
+import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+import java.awt.image.BufferedImage;
 public class Utilities {
     public static byte[] LoadImage(String filePath) throws Exception {
         File file = new File(filePath);
@@ -57,4 +60,47 @@ public class Utilities {
         LocalDateTime currentTime = LocalDateTime.of(year, month, day, hour, minute);
         return currentTime;
     }
+    public static byte[] convertPDFToByteArray(String path) throws FileNotFoundException {
+        File file = new File(path);
+        FileInputStream fis = new FileInputStream(file);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] data = new byte[(int) file.length()];
+        try {
+            int readNum = fis.read(data);
+            while (readNum != -1) {
+                bos.write(data, 0, readNum);
+                readNum = fis.read(data);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            System.exit((1));
+        }
+        data = bos.toByteArray();
+        return data;
+    }
+
+    public static void convertByteArrayToImage(byte[] data, String filename) throws IOException {
+        PDDocument document = PDDocument.load(data);
+        PDFRenderer pdfRenderer = new PDFRenderer(document);
+        for (int i = 0; i < document.getNumberOfPages(); i++) {
+            BufferedImage bim = pdfRenderer.renderImageWithDPI(i, 300, ImageType.RGB);
+            ImageIOUtil.writeImage(bim, String.format("/home/jmao/pdfconvert/%s-%d.%s", filename, i + 1, "jpg"), 300);
+        }
+        document.close();
+    }
+
+    public static void generateImages(String path) {
+        byte[] byteArray = null;
+        String filename = new File(path).getName();
+        try {
+            byteArray = convertPDFToByteArray(path);
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        try {
+            convertByteArrayToImage(byteArray, filename);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
 }
