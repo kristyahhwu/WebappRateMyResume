d7235d0d5df21327116f2c5e741dfd1eb1418d47
MichaelHan38 <michaelhan38@gmail.com>
2022-11-11 15:12:38 -0800

    Remove unnecessary comments; fix endpoint routes.

--- applications/server/src/main/java/SparkServer.java
+++ applications/server/src/main/java/SparkServer.java
@@ -7,7 +7,6 @@ public class SparkServer {
 
     public static void main(String[] args) {
         RoutesHandler handler = new RoutesHandler();
-
         port(4321);
 
         //Set CORS policy
@@ -17,26 +16,22 @@ public class SparkServer {
         get("/", handler.handleHome);
 
         get("/post/getAll", handler.handleGetAll);
-        get("/post/search", handler.handleSearch);// returns a collections of Posts, uses keyword
+        get("/post/search", handler.handleSearch);
         post("/post/create", handler.handleCreatePost);
 
-        get("/post/view", handler.handleViewPost); // will have postid in the query parameter.
-                                                        // See handleSearch for example, returns a single Post
-        put("/post/like", handler.handleLike); // will have postid, userid in the request body
-                                                        // (not query parameters).
-        put("/post/comment", handler.handleComment); // will have postid, user (the whole class object, not just id),
-                                                            // comment, time as request body.
+        put("/post/like", handler.handleLike);
+        put("/post/comment", handler.handleComment);
 
-        post("/user/login", handler.handleLogin); // Do this last. Might not need this at all
-        post("/user/logout", handler.handleLogout); // Do this last. Might not need this at all
+        get("/post/view", handler.handleViewPost);
+        get("/post/view/like", handler.handleNumberOfLikes);
+        get("/post/view/comments", handler.handleGetCommentsForPost);
+
+        post("/user/login", handler.handleLogin);
+        post("/user/logout", handler.handleLogout);
         post("/user/create/", handler.handleCreateUser);
 
         get("/demo/init", handler.handleInitDemo);
 
-        get("/post/comment", handler.handleGetCommentsForPost);// fetches all comments for a post whose postid is passed in
-                                                                // as a query parameter
-        get("/post/view/like", handler.handleNumberOfLikes); // retrieves number of likes for a post whose postid is passed in
-                                                                // as a query parameter
         //--------------------------------------------------------------------------------------------------------------
 
 
