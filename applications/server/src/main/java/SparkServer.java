9cce1262dd3f8a0101d46ef73a77d89b8f0e8c2f
jmao12 <jtnmga@gmail.com>
2022-11-11 14:14:02 -0800

    Clean up old commented out code and unused import statements

--- applications/server/src/main/java/SparkServer.java
+++ applications/server/src/main/java/SparkServer.java
@@ -1,31 +1,7 @@
-import static internal.service.RoutesHandler.*;
 import static spark.Spark.*;
 import static spark.Spark.before;
-
-
-import com.google.gson.Gson;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.*;
-
-import com.mongodb.client.gridfs.GridFSBucket;
-import com.mongodb.client.gridfs.GridFSBuckets;
-import com.mongodb.client.gridfs.model.GridFSUploadOptions;
-import internal.dataAccess.DTO.UserDTO;
-import internal.dataAccess.DTO.PostDTO;
 import internal.service.RoutesHandler;
-import internal.service.Utilities;
-import org.bson.Document;
-import org.bson.types.Binary;
-import org.bson.types.ObjectId;
+
 
 public class SparkServer {
 
@@ -33,26 +9,6 @@ public class SparkServer {
         RoutesHandler handler = new RoutesHandler();
 
         port(4321);
-
-//        // open connection
-//        MongoClientURI uri = new MongoClientURI("mongodb://admin:password@localhost:27017/");
-//        MongoClient mongoClient = new MongoClient(uri);
-//        // get ref to database
-//        MongoDatabase db = mongoClient.getDatabase("UsersDatabase");
-//        // get ref to collection
-//        MongoCollection<Document> teamMemberCollection = db.getCollection("teamMemberCollection");
-//        // get ref to users collection
-//        //db.createCollection("applicantsCollection");
-//        MongoCollection<Document> applicantsCollection = db.getCollection("applicantsCollection");
-//
-//        // get ref to post collections
-//        //db.createCollection("postsCollection");
-//        MongoCollection<Document> postsCollection = db.getCollection("postsCollection");
-//        // get ref to resume collection
-//        //db.createCollection("resumeCollection");
-//        MongoCollection<Document> resumeCollection = db.getCollection("resumeCollection");
-//
-//        Gson gson = new Gson();
 
         //Set CORS policy
         before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
