1cff2f990f519291cce6892400db10ab49768b7a
gaolarry1987 <gaolarry@yahoo.com>
2022-11-16 22:30:24 -0800

    finished numbeOfLikes Test

--- applications/server/src/test/java/internal/service/NumberOfLikesTest.java
+++ applications/server/src/test/java/internal/service/NumberOfLikesTest.java
@@ -1,40 +1,44 @@
 package internal.service;
 
 import com.google.gson.Gson;
-import internal.dataAccess.DTO.HandleCommentDTO;
 import internal.dataAccess.DTO.HandleLikeDTO;
 import internal.dataAccess.DTO.PostDTO;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class LikeTest {
-
+public class NumberOfLikesTest {
     Gson gson = new Gson();
+
     @Test
-    void like() {
-//        Like
-//Insert a post into the database
+    void numberOfLikes() {
+//        Get No. Of likes
+//        Insert a post with xx number of likes into the database
+//        Call getNoOfLikesForPost with the post ID
+//        Assert the number like is correct
+//        Delete the post and the like from the database
+
         PostDTO newPost = new PostDTO();
-        newPost.author = "Leiyi Gao";
+        newPost.postAuthor = "Leiyi Gao";
         newPost.title = "Leiyi Gao's resume";
         newPost.description = "Leiyi Gao's resume description";
+        newPost.filePath = "/Users/gaolarry/Downloads";
+        newPost.fileName = "Leiyi Gao.pdf";
         String newPostID = Utilities.createPost(gson.toJson(newPost));
 
-//        Call handleLike with the post ID
+        // Insert two likes to database for post
         HandleLikeDTO likeObj = new HandleLikeDTO();
         likeObj.postid = newPostID;
         likeObj.userid = String.valueOf(1000);
         String likeJson = gson.toJson(likeObj);
-        // Count number of likes before insertion
-        int numberLikesBefore = Utilities.countNumberOfLikesForPost(newPostID);
-
         String newLikeID = Utilities.like(likeJson);
-        assertFalse(newLikeID.startsWith("Error"));
+
+
+        Assert.assertFalse(newLikeID.startsWith("Error"));
 //        Assert the number like increases (should be 1)
-        int numberLikesAfter = Utilities.countNumberOfLikesForPost(newPostID);
-        assertTrue(numberLikesAfter > numberLikesBefore);
-        assertTrue( numberLikesAfter == 1);
+        int numberLikes = Utilities.countNumberOfLikesForPost(newPostID);
+        Assert.assertTrue( numberLikes == 1);
 //        Delete the post and the like from the database
         Utilities.deletePost(newPostID);
         Utilities.deleteLike(newLikeID);
